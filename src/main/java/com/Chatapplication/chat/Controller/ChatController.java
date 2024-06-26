41ddd569d91fa1b61555fecf9d8d805ccc4a8e5f
Abhi <aabhishek4008@gmail.com>
2024-06-26 19:50:33 +0530

    initial commit

---
+++ src/main/java/com/Chatapplication/chat/Controller/ChatController.java
@@ -0,0 +1,43 @@
+package com.Chatapplication.chat.Controller;
+
+import com.Chatapplication.chat.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    /**
+     * Registers a user for chat.
+     *
+     * param chatMessage The chat message containing the sender's information.
+     * param headerAccessor The SimpMessageHeaderAccessor object used to access session attributes.
+     * return The registered chat message.
+     */
+    @MessageMapping("/chat.register")
+    @SendTo("/topic/public")
+    public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+    @MessageMapping("/chat.sendToOne")
+    @SendTo("/topic/private.{recipientUsername}")
+    public ChatMessage sendToOne(@Payload ChatMessage chatMessage, @DestinationVariable String recipientUsername) {
+        return chatMessage;
+    }
+    /**
+     * Sends a chat message to all connected users.
+     *
+     * param chatMessage The chat message to be sent.
+     * return The sent chat message.
+     */
+    @MessageMapping("/chat.send")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+}
---
+++ src/test/java/com/Chatapplication/chat/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Chatapplication.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
